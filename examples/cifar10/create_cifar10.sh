3452aaea0a0593d1652278571fc79e6d7f4a1818
amanda <zhujianwang001@gmail.com>
2021-07-30 11:14:01 +0800

    更新训练训练配置文件

--- examples/cifar10/create_cifar10.sh
+++ examples/cifar10/create_cifar10.sh
@@ -9,11 +9,11 @@ echo "Creating $DBTYPE..."
 
 rm -rf $EXAMPLE/cifar10_train_$DBTYPE $EXAMPLE/cifar10_test_$DBTYPE
 
-./build/examples/cifar10/convert_cifar_data.bin $DATA $EXAMPLE $DBTYPE
+./cmake-build-debug/examples/cifar10/convert_cifar_data-d.bin $DATA $EXAMPLE $DBTYPE
 
 echo "Computing image mean..."
 
-./build/tools/compute_image_mean -backend=$DBTYPE \
+./cmake-build-debug/tools/compute_image_mean-d -backend=$DBTYPE \
   $EXAMPLE/cifar10_train_$DBTYPE $EXAMPLE/mean.binaryproto
 
 echo "Done."
